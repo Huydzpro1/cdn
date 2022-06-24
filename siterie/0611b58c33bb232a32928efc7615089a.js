34d81552bb072d83edc4c21403a749b1a38e60a4
Huydzpro1 <97621092+Huydzpro1@users.noreply.github.com>
2022-06-25 05:09:03 +0700

    Add files via upload

---
+++ siterie/0611b58c33bb232a32928efc7615089a.js
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title></title>
+</head>
+<body>
+  jQuery(document).ready(function($){$(function(){var
+  pull=$('#pull');menu=$('nav
+  ul');menuHeight=menu.height();$(pull).on('click',function(e){e.preventDefault();menu.slideToggle();});});})
+</body>
+</html>
